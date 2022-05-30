10cc3c93dd6e4084afb89cf53fb4408cbb5d48e6
addisLiaoCz <106232358+addisLiaoCz@users.noreply.github.com>
2022-05-30 18:12:16 +0800

    5.30 权限控制

--- contracts/abstract/HotpotERC20Base.sol
+++ contracts/abstract/HotpotERC20Base.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-// diy
+// diy addis
 import "./SwapCurve.sol";
 import "./HotpotMetadata.sol";
 
